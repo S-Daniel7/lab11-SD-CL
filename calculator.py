f98cfa66dcb4c7e7bdfe4ba619b3570c36a94001
Sophie Daniel <sophie.kelati@gmail.com>
2025-11-12 11:07:19 -0500

    merged changes

--- calculator.py
+++ calculator.py
@@ -1,3 +1,4 @@
+import math
 """
 calculator.py
 - Defines functions used to create a simple calculator
@@ -5,7 +6,22 @@ calculator.py
 One function per operation, in order.
 """
 # First example
-def add(a, b): 
-    pass
+def add(a, b):
+    return a + b
+def sub (a,b):
+    return a - b
+def mul (a,b):
+    return a * b
+def div(a,b):
+    try:
+       return b/a
+    except ZeroDivisionError:
+        print("Cannot divide by Zero")
+def log (a,b):
+    try:
+        return math.log(a,b)
+    except ValueError:
+        print("Number is not valid")
 
-
+def exp (a,b):
+    return math.pow(a,b)
